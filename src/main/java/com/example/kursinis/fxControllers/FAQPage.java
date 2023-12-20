94ebe4e07f192e943c4ca9230ca369683640ff3b
KarmaIsHere <letrixswitch@gmail.com>
2023-12-20 02:25:38 +0200

    added faq

--- src/main/java/com/example/kursinis/fxControllers/FAQPage.java
+++ src/main/java/com/example/kursinis/fxControllers/FAQPage.java
@@ -4,14 +4,16 @@ import com.example.kursinis.utilities.FxUtils;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FAQPage {
 
     public Button goBackB;
     public TextArea text;
+    public TextField textas;
 
 
     public void goBack(ActionEvent actionEvent) {
-        FxUtils.openFxPage("FAQ.fxml", text);
+        FxUtils.openFxPage("login-page.fxml", textas);
     }
 }
